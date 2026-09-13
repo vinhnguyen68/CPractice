246572baac122f5ccb8505f77d8c1d1f493703fc
Vinh Nguyen <vincenguyen262@gmail.com>
2026-09-13 14:43:23 -0700

    Start DocGia.h

---
+++ src/DocGia.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Ngay.h"
+#include <string>
+
+class DocGia
+{
+protected:
+	string ten;
+	Ngay ngay_lap_the;
+	int sothanghieuluc;
+public:
+	DocGia(void);
+	~DocGia(void);
+};
